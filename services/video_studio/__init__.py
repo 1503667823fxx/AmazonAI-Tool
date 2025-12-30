cf72daf073cbe0cce21969704b20080887ff4687
1503667823fxx <139320976+1503667823fxx@users.noreply.github.com>
2025-12-29 18:01:01 -0800

    Update __init__.py

--- services/video_studio/__init__.py
+++ services/video_studio/__init__.py
@@ -1,1 +1,24 @@
 
+"""
+Video Studio Services
+
+视频生成相关的服务模块
+"""
+
+from .veo_service import (
+    VeoAPIService,
+    get_veo_service,
+    generate_video_sync,
+    get_video_status_sync,
+    generate_video_async,
+    get_video_status_async
+)
+
+__all__ = [
+    'VeoAPIService',
+    'get_veo_service', 
+    'generate_video_sync',
+    'get_video_status_sync',
+    'generate_video_async',
+    'get_video_status_async'
+]
