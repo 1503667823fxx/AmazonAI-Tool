d2fa24625791509d5ea80af65531e64d65689346
1503667823fxx <139320976+1503667823fxx@users.noreply.github.com>
2026-01-01 01:26:58 -0800

    Update __init__.py

--- services/video_studio/__init__.py
+++ services/video_studio/__init__.py
@@ -8,10 +8,7 @@ from .veo_service import (
     VeoAPIService,
     get_veo_service,
     generate_video_sync,
-    get_video_status_sync,
-    download_video_with_progress,
-    get_cached_video,
-    clear_video_cache
+    get_video_status_sync
 )
 
 from .image_processor import (
@@ -25,9 +22,6 @@ __all__ = [
     'get_veo_service',
     'generate_video_sync', 
     'get_video_status_sync',
-    'download_video_with_progress',
-    'get_cached_video',
-    'clear_video_cache',
     'get_image_processor',
     'process_image_for_video_generation',
     'validate_uploaded_image'
